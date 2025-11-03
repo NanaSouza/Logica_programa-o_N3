3dd043f006dafe75ec7869583c0bf12d71f885cf
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2025-11-03 20:42:21 -0300

    primeiro commit com codigo

---
+++ Aula 1 Logica da programação/Program.cs
@@ -0,0 +1,29 @@
+﻿// declarar variavel nome
+String nome;
+
+nome = "nana souza";
+
+Console.WriteLine ("nana souza");
+
+//declarar variavel contante e atribuir valor a ela
+const string constante = "minha constante";
+Console.WriteLine(constante);
+
+int idade = 26;
+float alturafloat = 1.8f;
+double altura = 1.8;
+char letra = 'A';
+Boolean = false;
+bool = true;
+
+
+// tipo nome = valor;
+
+// Operadores: + ; - ; * ; / ;
+// Atribuição =
+// 
+
+
+
+
+
