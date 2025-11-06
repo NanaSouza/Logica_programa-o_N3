a807af64b8ca3914c9a4f28cd092b09072e8fc73
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2025-11-05 21:58:31 -0300

    Revisão aula

---
+++ Revisão aula 5/Program.cs
@@ -0,0 +1,37 @@
+﻿
+{
+    int n1;
+    int n2;
+    Console.WriteLine("Informe um numero inteiro.");
+    n1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe mais um numero inteiro.");
+    n2 = int.Parse(Console.ReadLine());
+
+   Console.WriteLine(n1 + n2);
+ 
+
+}
+
+{
+    int n1;
+    int n2;
+    Console.WriteLine("Informe um numero inteiro.");
+    n1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe mais um numero inteiro.");
+    n2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(n1 - n2);
+  
+
+}
+
+
+    int n1;
+    int n2;
+    int soma = n1 + n2;
+    int sub = n1 - n2;
+
+    Console.WriteLine("A soma é: " + soma);
+    Console.WriteLine("A sub é: " + sub);
