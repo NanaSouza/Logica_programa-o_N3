99948758cb536bead23987518c2b7587d9a2b596
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2025-11-19 21:59:29 -0300

    Aula 9

---
+++ Excercicios aula 9/Program.cs
@@ -0,0 +1,48 @@
+﻿//Tabuada: Faça um programa que receba um número e, usando laços de repetição, calcule e mostre a tabuada desse número.
+Console.WriteLine("Informe um numero");
+int Num = int.Parse(Console.ReadLine());
+int count = 0;
+int x = 1;
+
+do
+{
+    
+    Console.WriteLine(Num + "x" + x + "=" + (Num * x));
+
+    x++;
+    count++;
+
+}while(count <= 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Validação de Nota: Faça um programa que peça uma nota entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+
+
+//Controle de peso: Faça um programa que receba a idade e o peso de sete pessoas. Calcule e mostre:
+//A quantidade de pessoas com mais de 90 quilos;
+//A média das idades das sete pessoas;
