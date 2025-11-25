e264d174d384569338798e1e6aa73e0605bdf4b6
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2025-11-24 21:46:43 -0300

    Aula 10 +atividade

---
+++ Atividade aula 10/Program.cs
@@ -0,0 +1,41 @@
+﻿//Exercício 1 – Validação de Senha
+//Crie um programa que peça ao usuário uma senha. O programa deve permitir até 3 tentativas.
+//A senha correta é "12345".
+//Se o usuário digitar a senha correta E tiver mais de 18 anos (idade informada previamente), o acesso é liberado.
+//Caso contrário, o programa informa que o acesso foi negado.
+
+const int SENHA = 12345;
+const int IDADE = 18;
+
+for (int contador = 0; contador <3; contador++)
+{
+    Console.WriteLine("Informe a senha correta:");
+    string entradaUsuario = Console.ReadLine();
+    int.TryParse(entradaUsuario, out int senha);
+
+    Console.WriteLine("Informe a sua idade:");
+    string entradaUsuario1 = Console.ReadLine();
+    int.TryParse(entradaUsuario1, out int idade);
+
+    if (senha == SENHA &&  idade >= IDADE)
+    {
+        Console.WriteLine("Acesso Permitido");
+    }
+    else
+    {
+        Console.WriteLine("Acesso negado");
+    }  
+}
+
+//Exercício 2 – Números Pares e Ímpares
+//Peça ao usuário um número inteiro positivo. O programa deve imprimir todos os números de 1 até esse número.
+//Para cada número, informe se ele é par ou ímpar.
+//Se o número for par e múltiplo de 5 ao mesmo tempo, destaque com a mensagem "Par e múltiplo de 5".
+
+
+
+//Exercício 3 – Caixa Registradora
+//Simule uma caixa registradora simples.
+//O programa deve pedir o valor de cada produto até que o usuário digite 0 para encerrar.
+//Ao final, mostre o total da compra.
+//Se o total for maior que R$ 100 OU o cliente tiver um cupom de desconto (informado como CUPOM10), aplique 10% de desconto.
--- Excercicios aula 9/Program.cs
+++ Excercicios aula 9/Program.cs
@@ -1,38 +1,36 @@
 ﻿//Tabuada: Faça um programa que receba um número e, usando laços de repetição, calcule e mostre a tabuada desse número.
-Console.WriteLine("Informe um numero");
-int Num = int.Parse(Console.ReadLine());
-int count = 0;
-int x = 1;
+//Console.WriteLine("Informe um numero");
+//int Num = int.Parse(Console.ReadLine());
+//int count = 0;
+//int x = 1;
 
-do
-{
-    
-    Console.WriteLine(Num + "x" + x + "=" + (Num * x));
+//do
+//{
 
-    x++;
-    count++;
+//    Console.WriteLine(Num + "x" + x + "=" + (Num * x));
 
-}while(count <= 10);
+//    x++;
+//    count++;
 
+//}while(count <= 10);
 
+//Console.WriteLine("Informe um número");
+//string entradaUsuario = Console.ReadLine();
+//bool valido = int.TryParse(entradaUsuario, out int numero);
 
+//if(valido)
+//{
 
+//    for (int contador = 1; contador > 0; contador++) 
+//    {
+//        Console.WriteLine(contador + " * " + numero + " = " + (contador * numero));
+//    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//}
+//else
+//{
+//    Console.WriteLine("O valor informado não é valido");
+//}
 
 
 
@@ -41,8 +39,53 @@ do
 
 //Validação de Nota: Faça um programa que peça uma nota entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 
+//bool invalido = true;
+//do
+//{
+//    Console.WriteLine("insira uma nota entra 0 e 10:");
+//    string entradaUsuario = Console.ReadLine();
+
+//    bool valido = int.TryParse(entradaUsuario, out int nota);
+
+//    if (!valido || nota < 0 || nota > 10) 
+//    {
+//        Console.WriteLine("Informe uma nota valida");
+
+//    }
+//    else
+//    {
+//        Console.WriteLine(nota + "nota valida");
+//    }
+//} while (invalido);
 
 
 //Controle de peso: Faça um programa que receba a idade e o peso de sete pessoas. Calcule e mostre:
 //A quantidade de pessoas com mais de 90 quilos;
 //A média das idades das sete pessoas;
+//using System.Reflection.PortableExecutable;
+
+//const int QTD_PESSOAS = 7;
+//int contadorPessoas90kg = 0;
+//int somaIdadePessoas = 0;
+
+//string entradaUsuario;
+
+//for (int contador = 0; contador < QTD_PESSOAS; contador++)
+//{
+//    Console.WriteLine("Informe sua idade:");
+//    entradaUsuario = Console.ReadLine();
+//    int.TryParse(entradaUsuario, out int idade);
+
+//    Console.WriteLine("Informe seu peso:");
+//    entradaUsuario = Console.ReadLine();
+//    int.TryParse(entradaUsuario, out int peso);
+
+//    somaIdadePessoas += idade;
+
+//    if (peso > 90)
+//    {
+//        contadorPessoas90kg++;
+//    }
+//}
+//Console.WriteLine("Pessoas com mais de 90kg:" + contadorPessoas90kg);
+//Console.WriteLine("A media das idades é:" + (somaIdadePessoas / QTD_PESSOAS));
