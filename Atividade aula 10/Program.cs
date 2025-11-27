b60d8b641bc58dee62e09dd556e4d7f44120ea86
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2025-11-26 21:40:54 -0300

    Aula 11

--- Atividade aula 10/Program.cs
+++ Atividade aula 10/Program.cs
@@ -4,6 +4,8 @@
 //Se o usuário digitar a senha correta E tiver mais de 18 anos (idade informada previamente), o acesso é liberado.
 //Caso contrário, o programa informa que o acesso foi negado.
 
+using System.ComponentModel.Design;
+
 const int SENHA = 12345;
 const int IDADE = 18;
 
@@ -27,10 +29,74 @@ for (int contador = 0; contador <3; contador++)
     }  
 }
 
+
+
+
+const string SENHA_CORRETA = "12345";
+
+Console.WriteLine("Insira a sua idade:");
+bool idadeValida = int.TryParse(Console.ReadLine(), out int idade);
+
+if (!idadeValida)
+{
+    Console.WriteLine("Idade invalida!");
+}
+else
+{
+    int tentativas = 0;
+    string senha;
+    
+    do
+    {
+        Console.WriteLine("Insira a senha: (tentativas" + tentativas + ")");
+        tentativas++;
+        senha = Console.ReadLine();
+
+        if (senha == SENHA_CORRETA && idade >= 18)
+        {
+            Console.WriteLine("Acesso Liberado...");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado...");
+        }
+    }while (tentativas <3 && senha != SENHA_CORRETA);
+}
+
 //Exercício 2 – Números Pares e Ímpares
 //Peça ao usuário um número inteiro positivo. O programa deve imprimir todos os números de 1 até esse número.
 //Para cada número, informe se ele é par ou ímpar.
 //Se o número for par e múltiplo de 5 ao mesmo tempo, destaque com a mensagem "Par e múltiplo de 5".
+
+Console.WriteLine("Informe um numero inteiro positivo");
+bool inteiro = int.TryParse(Console.ReadLine(),out int numero);
+
+if (inteiro && numero > 0)
+{
+    for (int contador = 1; contador < numero; contador++)
+    {
+        bool par = contador % 2 == 0;
+        bool multiplo5 = contador % 5 == 0;
+
+        if (par && multiplo5)
+        {
+            Console.WriteLine(contador + " - é par e multplo de 5");
+        }
+        else if (par && !multiplo5)
+        {
+            Console.WriteLine(contador + "- é par");
+        }
+        else
+        {
+            Console.WriteLine(contador + "- é impar");
+        }
+    }
+    
+}
+else
+{
+    Console.WriteLine("O numero informado não é valido");
+}
 
 
 
@@ -39,3 +105,28 @@ for (int contador = 0; contador <3; contador++)
 //O programa deve pedir o valor de cada produto até que o usuário digite 0 para encerrar.
 //Ao final, mostre o total da compra.
 //Se o total for maior que R$ 100 OU o cliente tiver um cupom de desconto (informado como CUPOM10), aplique 10% de desconto.
+const string CUPOM10 = "CUPOM10";
+double valor = -1;
+double total =0;
+
+while (valor != 0)
+{
+    Console.WriteLine("Informe o valor dos produto:");
+    valor = double.Parse(Console.ReadLine());
+    if (valor > 0)
+    {
+        total += valor;
+    }
+}
+
+Console.WriteLine("Insira o cupom de desconto:");
+string cupom = Console.ReadLine();
+
+if (cupom == CUPOM10 || total > 100)
+{
+    Console.WriteLine("O valor total da compra com 10% de desconto é:" + (total * 0.9));
+}
+else
+{
+    Console.WriteLine("O valor total é R$" + total);
+}
---
+++ Aula 11/Program.cs
@@ -0,0 +1,47 @@
+﻿//Notas
+
+//int[] notas = new int[2];
+
+//Console.WriteLine("Notas p1");
+//notas[0] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota p2");
+//notas[1] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota final:" + ((notas[0] + notas[1]) / 2));
+
+
+
+
+//List<int> notas = new List<int>();
+
+//Console.WriteLine("Nota p1:");
+//notas.Add(int.Parse(Console.ReadLine()));
+
+//Console.WriteLine("Nota p2:");
+//notas.Add(int.Parse(Console.ReadLine()));
+
+//Console.WriteLine("Nota Final:" + ((notas[0] + notas[1] /2)));
+
+//for (int i = 0; i < notas.Count; i++)
+//{
+//    Console.WriteLine(notas[i]);
+//}
+
+Random random = new Random();
+List<int> numeros = new();
+for (int i = 0; i < random.Next(1, 10001); i++)
+{
+    numeros.Add(random.Next(1, 101));
+}
+
+
+for (int i = 0; i < numeros.Count; i++)
+{
+    int num = numeros[i];
+    if (num % 2 == 0)
+    {
+        Console.WriteLine(num);
+    }
+}
+
